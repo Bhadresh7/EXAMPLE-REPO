c69c9c199c8b84c3c6e5b8a192778ff57cfbfcf5
Bhadresh7 <bhadreshpalani19@gmail.com>
2024-12-29 23:51:57 -0500

    Frontend session

---
+++ frontend/js/data.js
@@ -0,0 +1,148 @@
+// // const moviesData = {
+// //   cinema: {
+// //     name: "StarWorld Cineplex",
+// //     location: {
+// //       country: "India",
+// //       state: "Maharashtra",
+// //       city: "Mumbai",
+// //       address: {
+// //         street: "Plot 45, Film City Road",
+// //         zipCode: "400065",
+// //       },
+// //     },
+// //     movies: [
+// //       {
+// //         title: "Galactic Odyssey",
+// //         details: {
+// //           genre: ["Sci-Fi", "Adventure"],
+// //           releaseYear: 2023,
+// //           duration: "2h 45m",
+// //           ratings: {
+// //             IMDB: 8.9,
+// //             RottenTomatoes: 95,
+// //           },
+// //         },
+// //         cast: [
+// //           {
+// //             actor: {
+// //               name: "Aryan Mehta",
+// //               role: "Commander Vega",
+// //               awards: [
+// //                 { name: "Best Actor", year: 2022 },
+// //                 { name: "People's Choice Award", year: 2021 },
+// //               ],
+// //             },
+// //           },
+// //           {
+// //             actor: {
+// //               name: "Priya Kapoor",
+// //               role: "Dr. Lyra",
+// //               awards: [],
+// //             },
+// //           },
+// //         ],
+// //         crew: {
+// //           director: {
+// //             name: "Ananya Sharma",
+// //             moviesDirected: 12,
+// //             age: 45,
+// //           },
+// //           writers: [
+// //             { name: "Rajesh Khanna", scriptsWritten: 8 },
+// //             { name: "Rhea Sen", scriptsWritten: 6 },
+// //           ],
+// //           producers: [{ name: "Karan Oberoi", moviesProduced: 25 }],
+// //         },
+// //         reviews: [
+// //           {
+// //             reviewer: "Sci-Fi Journal",
+// //             rating: 4.7,
+// //             comments: "An exhilarating journey through the stars!",
+// //           },
+// //         ],
+// //       },
+// //       {
+// //         title: "Love Beyond Borders",
+// //         details: {
+// //           genre: ["Romance", "Drama"],
+// //           releaseYear: 2022,
+// //           duration: "2h 15m",
+// //           ratings: {
+// //             IMDB: 7.5,
+// //             RottenTomatoes: 88,
+// //           },
+// //         },
+// //         cast: [
+// //           {
+// //             actor: {
+// //               name: "Ravi Singh",
+// //               role: "Raj",
+// //               awards: [{ name: "Best Debut", year: 2021 }],
+// //             },
+// //           },
+// //           {
+// //             actor: {
+// //               name: "Sneha Iyer",
+// //               role: "Aisha",
+// //               awards: [],
+// //             },
+// //           },
+// //         ],
+// //         crew: {
+// //           director: {
+// //             name: "Vikram Desai",
+// //             moviesDirected: 8,
+// //             age: 50,
+// //           },
+// //           writers: [{ name: "Meera Patil", scriptsWritten: 10 }],
+// //           producers: [{ name: "Arjun Kapoor", moviesProduced: 15 }],
+// //         },
+// //         reviews: [
+// //           {
+// //             reviewer: "Romance Times",
+// //             rating: 4.2,
+// //             comments: "A touching story that resonates with all!",
+// //           },
+// //           {
+// //             reviewer: "Drama Review Daily",
+// //             rating: 4.0,
+// //             comments: "A beautifully shot love story.",
+// //           },
+// //         ],
+// //       },
+// //     ],
+// //   },
+// // };
+
+// // movie name ;
+// // movie reivews
+// // movie actor
+
+// // result();
+// // const result = function () {
+// //   console.log("This is an anonoymous function");
+// // };
+
+// //anonymous functions
+
+// // (function () {
+// //   console.log("Another example");
+// // })();
+
+// // //immedietaly invoked functions
+
+// //arrow functions
+// // const arr = () => {
+// //     console.log("example for arrow function");
+// // };
+// // arr();
+
+// // (()=>{
+// //     console.log("example for IIF")
+// // })()
+
+// // var a = 10;
+// function name(){
+//     var c = 20;
+// }
+// console.log(c)
---
+++ frontend/js/script.js
@@ -0,0 +1,77 @@
+// var a = "20";
+// var b = 10;
+// console.log(a !== b);
+
+// var , let ,const
+
+// {
+//   var variable1 = 10;
+//   let variable2 = 20;
+//   const varibale3 = 30;
+//   var variable1 = 90;
+// //   let variable2 = 50;
+// const varibale3=80;
+
+//   console.log(variable1);
+//   console.log(variable2);
+//   console.log(varibale3);
+// }
+
+// var document=10;
+// console.log(document);
+
+// var str=new String("8");
+// console.log(str instanceof String)
+
+// console.log(!!"hello");
+// // Using ??
+// let userPreferredTheme = null;
+// let defaultTheme = "";
+
+// let appliedTheme = userPreferredTheme ?? defaultTheme;
+
+// console.log(appliedTheme);
+
+// var a = 35 - "4"; //type coercion
+// console.log(typeof a)
+
+
+// //type conversion   
+// console.log(Boolean(0)); // Output: false
+// console.log(Boolean(1)); // Output: true
+// console.log(Boolean("")); // Output: false
+// console.log(Boolean("Hello")); // Output: true
+
+// var e=1;
+// switch(e){
+//     case 1:
+//         console.log("This is 1");
+//         break
+//     case 10:
+//         console.log("this is 10")
+//         break
+//     default:
+//         console.log('nothing matched')
+//         break
+
+
+// for(let i=0;i<10;i++){
+//     if(i===5){
+//         break
+//     }
+//     console.log(i)
+// }
+
+
+
+var b ="This is an example for string methods in js";
+// console.log(b.length)
+// console.log(b.indexOf("is","8"))
+// console.log(b.lastIndexOf("is"))
+console.log(b.substring(4,1))
+// console.log(b.toLowerCase())
+// console.log(b.toUpperCase())
+console.log(b.slice(6,2))
+
+
+
